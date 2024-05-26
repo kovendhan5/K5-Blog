d6bacbe028cb4340351dce9cffea4083ee8db9b9
kovendhan5 <kovendhanofficial5@gmail.com>
2024-05-26 12:40:51 +0530

    create sign up

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,57 @@
+import { Button, Label, TextInput } from 'flowbite-react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Signup() {
-  return (
-    <div>
-      Signup
+  return (<div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        {/*left side*/}
+        <div className='flex-1'>
+        <Link
+       to="/"
+       className='font-bold dark:text-white text-4xl'
+       >
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500
+         via-purple-500 to-pink-500 rounded-lg text-white'>
+            K5
+        </span>
+        Blog
+      </Link>
+      <p className='text-sm mt-5'>
+        this is demo project.
+      </p>
+          </div>
+          {/*right side*/}
+
+          <div className='flex-1'>
+            <form className='flex flex-col gap-4'>
+              <div>
+                <Label value='your username' />
+                <TextInput type='text' placeholder='Username' id='username' />
+              </div>
+              <div>
+                <Label value='your email' />
+                <TextInput type='text' placeholder='example@gmail.com' id='email' />
+              </div>
+              <div>
+                <Label value='your password' />
+                <TextInput type='text' placeholder='Password' id='password' />
+              </div>
+              <Button> <span className='px-2 py-1 bg-gradient-to-r from-indigo-500
+         via-purple-500 to-pink-500 rounded-lg text-white'>
+            sign up
+        </span>
+              </Button>
+            </form>
+            <div className='flex gap-2 text-sm mt-5'>
+              <span>Have an account?</span>
+              <Link to='/sign-in' className='text-blue-500'>
+                sign In
+              </Link>
+            </div>
+
+        </div>
+      </div>
     </div>
   )
 }
